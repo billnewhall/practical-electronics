277e828974afc3531a310d798a73bb7248adc3b7
billnewhall <billnewhall@yahoo.com>
2023-05-06 13:58:55 -0600

    Moved run-once code to setup function.

--- Basic_PWM/Basic_PWM.ino
+++ Basic_PWM/Basic_PWM.ino
@@ -12,13 +12,14 @@
 
 // This code runs once at startup
 void setup() {
+  byte duty_cycle;  // Declare an unsigned byte to use for duty cycle (0 to 255)
   Serial.begin(115200);             // Initialize the serial port for the Serial Monitor
   pinMode(PWM_OUTPUT_PIN, OUTPUT);  // Set the pin to be an output
+  duty_cycle = 100;                 // Duty cycle (valid range is 0 to 255)
+  analogWrite(PWM_OUTPUT_PIN, duty_cycle );   // Output a PWM signal on the PWM_OUTPUT_PIN
 }
 
 // This code runs repeatedly
 void loop() {
-  byte duty_cycle;
-  duty_cycle = 100;   // Duty cycle (valid range is 0 to 255)
-  analogWrite(PWM_OUTPUT_PIN, duty_cycle );   // Output a PWM signal on the PWM_OUTPUT_PIN
+  // There is nothing to run repeatedly for this program
 }
