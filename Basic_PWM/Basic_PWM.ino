56ad87765e749fd11336977b8ab8f3e3b5f41fd9
billnewhall <billnewhall@yahoo.com>
2023-05-06 14:10:18 -0600

    Added text output for Serial Monitor

--- Basic_PWM/Basic_PWM.ino
+++ Basic_PWM/Basic_PWM.ino
@@ -12,11 +12,17 @@
 
 // This code runs once at startup
 void setup() {
-  byte duty_cycle;  // Declare an unsigned byte to use for duty cycle (0 to 255)
+  byte duty_cycle;                  // Declare an unsigned byte to use for duty cycle (0 to 255)
   Serial.begin(115200);             // Initialize the serial port for the Serial Monitor
   pinMode(PWM_OUTPUT_PIN, OUTPUT);  // Set the pin to be an output
   duty_cycle = 100;                 // Duty cycle (valid range is 0 to 255)
-  analogWrite(PWM_OUTPUT_PIN, duty_cycle );   // Output a PWM signal on the PWM_OUTPUT_PIN
+
+  analogWrite(PWM_OUTPUT_PIN, duty_cycle );     // Output a PWM signal on the PWM_OUTPUT_PIN
+
+  Serial.print("Creating PWM output on pin ");  // Output descriptive text to the Serial Monitor
+  Serial.print(PWM_OUTPUT_PIN);
+  Serial.print(" with a duty cicle of ");
+  Serial.println(duty_cycle);
 }
 
 // This code runs repeatedly
